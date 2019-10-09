ef3243c04c91b805189d251f3844f8052d546139
memorvin <alvina.tjia@yahoo.com>
2019-10-10 00:23:28 +0700

    edit exercise3

--- exercise3.js
+++ exercise3.js
@@ -1,19 +1,5 @@
 //Logic Challenge - Makan Terus (Rekursif)
 
-Seorang customer sedang makan di sebuah restaurant AYCE(All You Can Eat).
-Dia akan terus makan sampai waktu yang diberikan oleh restaurant tersebut habis. 
-etiap customer memesan/mengambil makanan dan memakan pesanannya maka waktu customer berkurang 15 menit.
-Begitu seterusnya sampai waktunya habis .Customer masih bisa memesan/mengambil makanan selama waktunya belum 0.
-
-Diberikan sebuah function makanTerusRekursif(waktu) yang menerima satu parameter berupa waktu.
-Function akan memproses jika waktu masih ada. Function akan mengembalikan/me-return berapa kali seorang customer
-memesan/mengambil makanan dalam waktu yang diberikan. Wajib menggunakan rekursif.
-
-Contoh:
-45 => 3
-100 => 7
-10 => 1
-
 function makanTerusRekursif(waktu) {
   if (waktu < 15 && waktu > 1) {
     return 1;
